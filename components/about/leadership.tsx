4aa7022013ff2e57206a799fd9d5c773edfcada5
Sahu <minaketana.b@rently.com>
2026-07-06 12:05:03 +0530

    Founders made it center

--- components/about/leadership.tsx
+++ components/about/leadership.tsx
@@ -56,7 +56,7 @@ export function Leadership() {
           </p>
         </motion.div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="flex justify-center">
           {leaders.map((leader, index) => (
             <motion.div
               key={leader.name}
